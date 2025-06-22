e2bb638d6a7c17c1db2e81b5eb3b3ca9e34061f1
anshpal3924 <ap23260013@gmail.com>
2025-06-22 22:24:33 +0530

    pushing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-
-
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AppRouter from './components/AppRouter';
@@ -12,47 +10,19 @@ import { CartProvider } from './contexts/CartContext';
 import PopupModal from './components/PopupModal';
 import LeadCaptureModal from './components/LeadCaptureModal';
 
-<<<<<<< HEAD
-=======
-import useFacebookPixel from './hooks/useFacebookPixel';
-import FloatingWhatsAppButton from './components/FloatingWhatsAppButton';
 
 
-function AppWithTracking() {
-  useFacebookPixel();
-
-  return (
-    <>
-      <ScrollToTop />
-      <AuthProvider>
-        <CartProvider>
-          <AnalyticsTracker />
-          <PopupModal />
-          <LeadCaptureModal />
-          <Header />
-          <main>
-            <AppRouter />
-          </main>
-          <Footer />
-        </CartProvider>
-        <FloatingWhatsAppButton />
-      </AuthProvider>
-    </>
-  );
-}
->>>>>>> 1308bdc34c4244fa9fd65eadde2f188da6ccb39d
 
 function App() {
   return (
     <HelmetProvider>
       <Router>
-<<<<<<< HEAD
         <ScrollToTop />
         <AuthProvider>
           <CartProvider>
             <AnalyticsTracker />
+         
             <PopupModal />
-       
             <LeadCaptureModal />
             <Header />
             <main>
@@ -61,14 +31,10 @@ function App() {
             <Footer />
           </CartProvider>
         </AuthProvider>
-=======
-        <AppWithTracking />
->>>>>>> 1308bdc34c4244fa9fd65eadde2f188da6ccb39d
       </Router>
     </HelmetProvider>
   );
 }
-
 
 // Add ScrollToTop component to ensure pages start at the top
 function ScrollToTop() {
